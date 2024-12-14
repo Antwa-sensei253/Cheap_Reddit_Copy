560725b5e219f828ce464dbdf3bc0951a1a446d9
Antwa-sensei253 <ahmed.qwerty253@gmail.com>
2024-12-14 09:07:44 +0200

    ch pass finish

--- accounts/url.py
+++ accounts/url.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='change_pass'),
     path('logout/', LogoutViewCustom.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('change_password/', auth_views.PasswordChangeView.as_view(template_name='accounts/change_pass.html'), name='change_password'),
+
+    path('change_password/done/', auth_views.PasswordChangeDoneView.as_view(template_name = 'change_pass_done.html'),name ='password_change_done')
 ]
--- discuss_board/settings.py
+++ discuss_board/settings.py
@@ -130,6 +130,7 @@ STATICFILES_DIRS = [
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
